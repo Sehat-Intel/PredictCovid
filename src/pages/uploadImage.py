6456a3a7a7907f4c95f1acfcb1edaae5de69cb30
Krunal Vasoya <krunalvasoya@gmail.com>
2020-11-18 20:04:49 +0530

    code optimization and debugging

--- src/pages/uploadImage.py
+++ src/pages/uploadImage.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  2 00:22:10 2020
-
-@author: tumin
-"""
-
 import numpy as np
 import streamlit as st
 import tensorflow as tf
@@ -18,36 +10,30 @@ import config as config
 Reports = config.db['records']
 Users = config.db['users']
 
-def import_and_predict(image_data, model):
-        size = (128,128)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = image.convert('RGB')
-        image = np.asarray(image)
-        image = (image.astype(np.float32) / 255.0)
+model = tf.keras.models.load_model('model.hdf5')
 
-        img_reshape = image[np.newaxis,...]
-
-        prediction = model.predict_classes(img_reshape)
-        prob = model.predict_proba(img_reshape)
-        return prediction, prob
-
-def predict(image):
-    model = tf.keras.models.load_model('model.hdf5')
-    prediction, prob = import_and_predict(image, model)
+def predict(image_data,model):
     
+    size = (128,128)    
+    img = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = img.convert('RGB')
+    img = np.asarray(img)
+    img = (img.astype(np.float32) / 255.0)
+    img_reshape = img[np.newaxis,...]
+
+    prediction = model.predict_classes(img_reshape)
+    prob = model.predict_proba(img_reshape)
+
     if prediction[0,0] == 0:
         st.warning("Covid-19")
         st.write("Probability : {:.2f}".format(((1-prob[0,0]))*100),"%")
-        st.image(image, use_column_width=True)
+        st.image(image_data, use_column_width=True)
         return "Covid","{:.2f} %".format((1-prob[0,0])*100)
     else:
         st.info("Normal")
         st.write("Probability : {:.2f}".format((prob[0,0])*100),"%")
-        st.image(image, use_column_width=True)
+        st.image(image_data, use_column_width=True)
         return "Normal","{:.2f} %".format((prob[0,0])*100)
-
-    
-
 
 def main(email):
     st.sidebar.write("""
@@ -59,7 +45,6 @@ def main(email):
     st.info("Our model has baseline accuracy of 94%")
     st.title("Predict the Probability of Covid-19 🧙‍♂️")
   
-    #st.warning("You have two options here, 1) Upload your X-ray 2) Test the app with test images")
     st.subheader("1) Upload your PA View X-ray 📤")
 
     st.set_option('deprecation.showfileUploaderEncoding', False)
@@ -73,7 +58,7 @@ def main(email):
             else:
                 image = Image.open(file)
                 base64string = imgto64.b64(file)
-                status, percent = predict(image)
+                status, percent = predict(image,model)
                 print(email)
                 user = Users.find_one({'email': email})
                 Reports.insert_one({
@@ -92,14 +77,14 @@ def main(email):
     if test_img == 'Image 1':
         image = Image.open('test_img/test.jpeg')
         if st.button("Test"):
-            predict(image)
+            predict(image,model)
         else:
             st.image(image, caption="Test Image 1" ,use_column_width=True)
         
     elif test_img == 'Image 2':
         image = Image.open('test_img/test1.jpg')
         if st.button("Test"):
-            predict(image)
+            predict(image,model)
         else:
             st.image(image, caption="Test Image 1" ,use_column_width=True)
 
